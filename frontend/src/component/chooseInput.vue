c3cc1664310bd258ffc12acfbde3f571bbde8504
huzongyao <hzy3774@gmail.com>
2017-03-15 13:15:55 +0800

    add frontend code

---
+++ frontend/src/component/chooseInput.vue
@@ -0,0 +1,62 @@
+<style lang="scss" scoped>
+    #medicalExpense{
+        height: 100%;
+        background: white;
+        li{
+            border-top:    1px solid #d3d3d3;
+            border-bottom: 1px solid #d3d3d3;
+            span{
+                padding-left: 50px;
+                display: inline-block;
+                box-sizing: border-box;
+            }
+            span:first-child{
+                width: 400px;
+                border-right: 1px solid #d3d3d3;
+            }
+            span:last-child{
+                width: calc(100% - 400px);
+            }
+        }
+    }
+</style>
+
+<template>
+    <div>
+        <el-select v-model="value" placeholder="请选择">
+            <el-option
+            v-for="item in options"
+            :label="item.label"
+            :value="item.value">
+            </el-option>
+        </el-select>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props:['options','getValue'],
+        data: function () {
+            return {
+                value: ''
+            }
+        },
+        components:{
+
+        },
+        methods:{
+        
+        },
+        watch:{
+            value:function(val,oldVal){
+                if(typeof this.getValue == 'function'){
+                    this.getValue(val);
+                }
+            }
+        },
+        mounted(){
+            console.log(12313131);
+        }
+    }   
+</script>
---
+++ frontend/src/homePage/homePage.vue
@@ -0,0 +1,47 @@
+<style lang="scss" scoped>
+    #homePage{
+        height: 100%;
+        background: white;
+        div.el-tabs{
+            height:100%;
+            div.el-tabs__content{
+                height: calc(100% - 57px);
+            }
+            div.el-tab-pane{
+                height: 100%;
+            }
+        }
+    }
+</style>
+
+<template>
+    <div id='homePage'>
+    <el-tabs v-model="activeTab" type="card" @tab-click="handleClick">
+        <el-tab-pane label="医疗费用" name="first">
+            <medicalExpense />
+        </el-tab-pane>
+        <el-tab-pane label="减免费用" name="second">减免费用</el-tab-pane>
+        <el-tab-pane label="补助费用" name="third">补助费用</el-tab-pane>
+    </el-tabs>
+    </div>
+</template>
+
+<script>
+    import api from '../backend/api';
+    import medicalExpense from './medicalExpense';
+    export default {
+        data: function () {
+            return {
+               activeTab: 'first'
+            }
+        },
+        components:{
+            medicalExpense
+        },
+        methods:{
+          handleClick(){
+
+          }
+        },
+    }   
+</script>
